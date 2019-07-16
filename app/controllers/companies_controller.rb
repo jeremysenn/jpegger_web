80cdc0bbee98c68bf8b755b71ebdc20507bbcee3
Jeremy <senn.jeremy@gmail.com>
2019-07-16 08:30:53 -0700

    Allow for updaing leads information from company edit page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -74,7 +74,7 @@ class CompaniesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def company_params
-      params.require(:company).permit(:name, :phone, :jpegger_service_ip, :jpegger_service_port, :yard_id)
+      params.require(:company).permit(:name, :phone, :jpegger_service_ip, :jpegger_service_port, :yard_id, :leads_online, :leads_online_store_id)
     end
     
 end
