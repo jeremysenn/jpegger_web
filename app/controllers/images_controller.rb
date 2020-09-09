2c0d695e7345432bf252afbcbbdbf40aaab40cfd
Jeremy <senn.jeremy@gmail.com>
2020-09-09 11:22:05 -0700

    Remove signature modal for now

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
-  before_action :set_image_field_descriptions, if: -> { current_user and not current_user.super? and FieldDescription.search_images_table?(current_user)}
+#  before_action :set_image_field_descriptions, if: -> { current_user and not current_user.super? and FieldDescription.search_images_table?(current_user)}
   before_action :authenticate_user!
   before_action :set_image, only: [:show, :edit, :update, :destroy]
   load_and_authorize_resource only: [:index]
