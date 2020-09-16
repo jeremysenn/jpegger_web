4787fae9e827d928df442867699ad2ed56fce081
Jeremy <senn.jeremy@gmail.com>
2020-09-15 17:13:54 -0700

    Postgres database; seed the database on first pass; Don't look at image file thumb version

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ActiveResource::Base
-  self.site = "http://localhost:3000/api/v1"
+#  self.site = "http://localhost:3000/api/v1"
+  self.site = "http://api:3000/api/v1"
   
   validates_presence_of :ticket_nbr
   validates_presence_of :file
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+super_company = Company.create(name: "Super Company")
+admin_company = Company.create(name: "Company Name")
+
+super_user = User.create(email: "superuser@tranact.com", first_name: "Super", last_name: "User", company_id: super_company.id, 
+  role: 'super', active: true, confirmed_at: Time.now, password: "Kolakola1", password_confirmation: "Kolakola1")
+
+admin_user = User.create(email: "admin@company.com", first_name: "Admin", last_name: "User", company_id: admin_company.id, 
+  role: 'admin', active: true, confirmed_at: Time.now, password: "kolakola", password_confirmation: "kolakola")
