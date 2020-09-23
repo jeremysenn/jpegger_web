7597f41045111bb539eb6965852388a702314ccc
Jeremy <senn.jeremy@gmail.com>
2020-09-23 08:28:09 -0700

    localhost for api resources when deploying to Azure

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveResource::Base
-  self.site = "http://localhost:3000/api/v1"
-#  self.site = "http://api:3000/api/v1"
+  self.site = "http://localhost:3000/api/v1" # Azure container group API
+#  self.site = "http://api:3000/api/v1" # Docker container group API
   
   validates_presence_of :ticket_nbr
   validates_presence_of :file
